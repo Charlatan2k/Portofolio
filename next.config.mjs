80153023c3c04baa1742954564e4a53a9e4f5d44
Charlatan2k <octavianmihai.calin@gmail.com>
2024-12-21 00:14:37 +0100

    update gitignore and do the build

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,14 @@
 const isProd = process.env.NODE_ENV === 'production'; // Check if we're in production (i.e., deployed)
 
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true,
   },
+  distDir: 'dist',
+  basePath: isProd ? '/my-portofolio' : '',
+  assetPrefix: isProd ? '/my-portofolio/' : '',
 
-  // Conditionally set basePath for production
-  basePath: isProd ? '/my-portofolio' : '', // Only apply basePath in production
-  assetPrefix: isProd ? '/my-portofolio/' : '', // Prefix assets in production only
-
-  output: 'export', // Ensure static export for GitHub Pages
+  output: 'export',
 };
 
 export default nextConfig;
