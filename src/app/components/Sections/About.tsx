54d8e40b7c64504f952544109c56b428c8963d6b
Charlatan2k <octavianmihai.calin@gmail.com>
2024-12-24 22:45:35 +0100

    feat: finish about section down to laptop bp

--- src/app/components/Sections/About.tsx
+++ src/app/components/Sections/About.tsx
@@ -8,11 +8,11 @@ export default function About() {
   return (
     <div
       id='about'
-      className='flex xl:mt-[30%] 2xl:mt-[18%] w-full flex-col justify-center items-center'
+      className='flex xl:mt-[30%] 2xl:mt-[18%] lg:mt-[40%] md:mt-[45%] w-full flex-col justify-center items-center'
     >
       <section
         id='#about'
-        className='border-[5px] flex flex-col items-center rounded-xl border-[var(--primary-gray)] w-[80%] h-[10%] pb-36'
+        className='border-[5px] flex flex-col items-center rounded-xl border-[var(--primary-gray)] 2xl:w-[80%] xl:w-[85%] lg:w-[90%] w-[70%] h-[10%] 3xl:h-[70rem] lg:pb-12 xl:pb-36'
       >
         <header className='titles-div justify-center items-center flex gap-6 w-full mt-44'>
           <hr className='border-[2px] border-[var(--primary-gray)] w-[50%] xl:w-[8%]' />
@@ -22,14 +22,14 @@ export default function About() {
           <hr className='border-[2px] border-[var(--primary-gray)] w-[50%] xl:w-[55%]' />
         </header>
         <article className='content-container flex flex-col w-full xl:mt-24 mt-10'>
-          <div className='content-duo 2xl:gap-[20%] gap-[20%] flex'>
-            <section className='flex items-center justify-center 2xl:gap-64 xl:gap-24'>
-              <section className='text-container flex ml-24 flex-col items-center 2xl:w-[40%] xl:w-[70%] relative'>
-                <h2 className='text-3xl font-medium '>
+          <div className='content-duo 2xl:gap-[20%] gap-[20%]  flex'>
+            <section className=' items-center flex flex-col lg:flex-row w-full justify-center lg:gap-14 xl:gap-24 2xl:gap-64 3xl:gap-64 '>
+              <section className='text-container lg:w-[70%] flex lg:ml-5 xl:ml-24 flex-col items-center 2xl:w-[40%] xl:w-[70%] relative'>
+                <h2 className='xl:text-3xl 3xl:text-3xl lg:text-2xl self-start  font-medium '>
                   {t('about_second_title')}
                 </h2>
                 <article className='flex flex-col p-8 border-l-4 border-t-4 border-[var(--primary-blue)] rounded-md'>
-                  <p className='font-light text-xl'>
+                  <p className='font-light lg:text-base 3xl:text-2xl xl:text-xl'>
                     {t('description1')}{' '}
                     <span className='font-bold text-[var(--primary-blue)]'>
                       JavaScript
@@ -69,12 +69,12 @@ export default function About() {
                   width={400}
                   height={400}
                   alt='About me Photo'
-                  className='mr-20'
+                  className=' lg:mr-10 3xl:w-[520px] xl:mr-20 3xl:mr-36'
                 />
               </figure>
             </section>
           </div>
-          <section className=' flex items-end p-8 mt-12 justify-between mb-[-7%] h-44 w-full '>
+          <section className=' flex items-end p-8 mt-12 justify-between 3xl:justify-evenly 3xl:gap-96 xl:mb-[-7%] h-44 2xl:h-32 3xl:h-44 w-full '>
             <CircularProgressBar percentage={85} text={t('progressCircle1')} />
             <CircularProgressBar percentage={75} text='Fitness' />
             <CircularProgressBar percentage={55} text={t('progressCircle3')} />
