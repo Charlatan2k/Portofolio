3227a88a15fdc0caad6404c28e66b7399dce2b6e
Octavian Calin <octavianmihai.calin@gmail.com>
2024-12-25 16:41:04 +0100

    feat: finish skills section for laptopL view

--- src/app/components/Sections/Skills.tsx
+++ src/app/components/Sections/Skills.tsx
@@ -31,7 +31,7 @@ export default function Skills() {
       <div className='border-[3px] border-[var(--primary-gray)] h-24'></div>
       <div
         id='skills-content-box'
-        className='border-[5px] border-[var(--primary-gray)] rounded-xl h-[48%] w-[80%] flex justify-center items-center'
+        className='border-[5px] border-[var(--primary-gray)] rounded-xl xl:h-[53%] 2xl:h-[48%] xl:w-[85%] 2xl:w-[80%] flex justify-center items-center'
       >
         <section className='flex flex-col border-r-[5px] border-[var(--primary-gray)] h-full justify-between w-[90%]'>
           <div className='flex flex-col items-center justify-center h-full'>
@@ -46,7 +46,7 @@ export default function Skills() {
                 Back
               </h1>
             </div>
-            <div className=' w-[85%] 3xl:w-[80%] h-auto flex gap-6 flex-wrap items-center justify-center mt-8'>
+            <div className=' xl:w-[95%] 2xl:w-[85%] 3xl:w-[80%] h-auto flex gap-6 flex-wrap items-center justify-center mt-8'>
               <Skill name='Node' />
               <Skill name='Express' />
               <Skill name='MySQL' />
