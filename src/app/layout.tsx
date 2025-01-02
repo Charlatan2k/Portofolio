ce311c45e1eb9777a0c3d5d1935c9c2ca9c3fbdc
Charlatan2k <octavianmihai.calin@gmail.com>
2025-01-02 15:56:37 +0100

    feat: remove non used fonts and add montserrat font google font import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Portofolio',
@@ -33,18 +21,11 @@ export default function RootLayout({
           crossOrigin='anonymous'
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'
           rel='stylesheet'
         />
-        <link rel='preconnect' href='//fdn.fontcdn.ir' />
-        <link rel='preconnect' href='//v1.fontapi.ir' />
-        <link href='https://v1.fontapi.ir/css/SFMono' rel='stylesheet' />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   );
 }
